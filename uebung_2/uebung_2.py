d5338d6d9676c40df1700eb21baba54d23f6cd6a
Urs <urs.gompper@gmail.com>
2020-05-05 18:35:44 +0200

    rectangle stays

--- uebung_2/uebung_2.py
+++ uebung_2/uebung_2.py
@@ -23,10 +23,11 @@ def draw(event,x,y,flags,parm):
             img = orig.copy()
             cv2.rectangle(img,start_pt,(x,y),(0,0,0),2)        
     if event == cv2.EVENT_LBUTTONUP:
-        drawr = False
         if drawr == True:
-            end_pt = x,y
-            cv2.rectangle(img,start_pt,end_pt,(0,0,0),2)
+            cv2.rectangle(img,start_pt,(x,y),(0,0,0),2)
+            #img_zoom = cv2.resize(img,fx=2,fy=2)
+            #cv2.imshow("img_zoom",img_zoom)
+            drawr = False
 
         
 
@@ -37,8 +38,6 @@ while True:
     cv2.setMouseCallback('img',draw)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-    
-# Show image
-#cv2.imshow('image',img)
-#cv2.waitKey(0)
+
+
 cv2.destroyAllWindows()
