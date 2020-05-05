74ef9f9835661cd8099a8802a3350c873835669f
Urs <urs.gompper@gmail.com>
2020-05-05 20:09:51 +0200

    open second window with cropped img

--- uebung_2/uebung_2.py
+++ uebung_2/uebung_2.py
@@ -14,25 +14,26 @@ img = orig.copy()
     
 
 def draw(event,x,y,flags,parm):
-    global drawr, start_pt,img
+    global drawr, xi,yi,img
     if event == cv2.EVENT_LBUTTONDOWN:
-        start_pt = x,y
+        xi,yi = x,y
         drawr = True
     if event == cv2.EVENT_MOUSEMOVE:
         if drawr == True:
             img = orig.copy()
-            cv2.rectangle(img,start_pt,(x,y),(0,0,0),2)        
+            cv2.rectangle(img,(xi,yi),(x,y),(0,0,0),2)        
     if event == cv2.EVENT_LBUTTONUP:
         if drawr == True:
-            cv2.rectangle(img,start_pt,(x,y),(0,0,0),2)
-            #img_zoom = cv2.resize(img,fx=2,fy=2)
-            #cv2.imshow("img_zoom",img_zoom)
+            cv2.rectangle(img,(xi,yi),(x,y),(0,0,0),2)
+            img_crop = img[xi:x,yi:y]
+            img_zoom = cv2.resize(img_crop,fx=0,fy=0,dsize=(abs(xi-x),abs(yi-y)))
+            cv2.imshow("img_zoom",img_zoom)
             drawr = False
 
         
 
 drawr = False
-start_pt = 0,0
+(xi,yi)=(0,0)
 while True:
     cv2.imshow("img", img)
     cv2.setMouseCallback('img',draw)
