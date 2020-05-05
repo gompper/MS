ae2fe39ad0d50553fabbde6774a12167b3e3507e
Urs <urs.gompper@gmail.com>
2020-05-05 18:16:44 +0200

    rectangle verschwindet

--- uebung_2/uebung_2.py
+++ uebung_2/uebung_2.py
@@ -6,23 +6,28 @@ Created on Tue May  5 13:45:00 2020
 """
 
 import cv2
+#import numpy as np
 
-img = cv2.imread('lena.bmp',cv2.IMREAD_GRAYSCALE) # 0 = Greyscale
+orig = cv2.imread('lena.bmp',cv2.IMREAD_GRAYSCALE) # 0 = Greyscale
 
-def draw_rectangle(start_pt,end_pt):
-    cv2.rectangle(img,start_pt,end_pt,(0,0,0),2)
+img = orig.copy()
+    
 
 def draw(event,x,y,flags,parm):
-    global drawr, start_pt
+    global drawr, start_pt,img
     if event == cv2.EVENT_LBUTTONDOWN:
         start_pt = x,y
         drawr = True
-        
+    if event == cv2.EVENT_MOUSEMOVE:
+        img = orig.copy()
+        if drawr == True:
+            cv2.rectangle(img,start_pt,(x,y),(0,0,0),-1)        
     if event == cv2.EVENT_LBUTTONUP:
+        drawr = False
         if drawr == True:
             end_pt = x+10,y+10
-            print("left")
-            draw_rectangle(start_pt,end_pt)
+            cv2.rectangle(img,start_pt,end_pt,(0,0,0),2)
+
         
 
 drawr = False
